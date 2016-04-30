67c2f584eba2c34c5078bd944c3ea427649525ef
tomislav989x <tomislav989s@gmail.com>
2016-04-30 18:10:46 +0200

    Update ChatroomEndPoint.java
    Dodan TextArea za prikaz tko je trenutno online tj. tko je spojen na server.

--- src/Moj2/ChatroomEndPoint.java
+++ src/Moj2/ChatroomEndPoint.java
@@ -2,48 +2,65 @@ package Moj2;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.*;
-import javax.websocket.server.ServerEndpoint;
-import javax.websocket.Session;
-import javax.websocket.OnOpen;
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonWriter;
-
-@ServerEndpoint("/ChatroomEndPoint")
-
-public class ChatroomEndPoint {
-    static Set<Session> ChatroomUsers = Collections.synchronizedSet(new HashSet<Session>());
-    @OnOpen
-    public void handleOpen(Session userSession){
-    	ChatroomUsers.add(userSession);
-    }
-    @OnClose
-    public void handleClose(Session userSession){
-    	ChatroomUsers.remove(userSession);
-    }
-    @OnMessage
-    public void handleMessage(String message, Session userSession) throws IOException {
-    	String username = (String) userSession.getUserProperties().get("userName");
-    	if(username == null){
-    		userSession.getUserProperties().put("userName", message);
-    		userSession.getBasicRemote().sendText(buildJsonData("System","You are connected as"+message));
-    	}else{
-    		Iterator<Session> iterator= ChatroomUsers.iterator();
-    		while(iterator.hasNext()){
-    			iterator.next().getBasicRemote().sendText(buildJsonData(username,message));
-    		}
-    	}
-    }
-	private String buildJsonData(String username, String message) {
-			JsonObject jsonObject = Json.createObjectBuilder().add("message", username + ":"+message).build();
-		    StringWriter stringWriter = new StringWriter();
-		    try (JsonWriter jsonWriter = Json.createWriter(stringWriter)){
-		    	jsonWriter.write(jsonObject);
-		    }
-			return stringWriter.toString();
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+@ServerEndpoint("/chatroomServerEndpoint")
+public class ChatroomServerEndpoint {
+	static Set<Session> chatroomUsers = Collections.synchronizedSet(new HashSet<Session>());
+	@OnOpen
+	public void handleOpen(Session userSession) throws IOException {
+		chatroomUsers.add(userSession);
+		Iterator<Session> iterator = chatroomUsers.iterator();
+        while (iterator.hasNext()) (iterator.next()).getBasicRemote().sendText(buildJsonUsersData());
+	}
+	@OnMessage
+	public void handleMessage(String message, Session userSession) throws IOException {
+		String username = (String) userSession.getUserProperties().get("username");
+		Iterator<Session> iterator = chatroomUsers.iterator();
+		if (username==null) {
+			userSession.getUserProperties().put("username", message);
+			userSession.getBasicRemote().sendText(buildJsonMessageData("System","you are now connected as "+message));
+			while (iterator.hasNext()) (iterator.next()).getBasicRemote().sendText(buildJsonUsersData());
+		} else {
+			
+			while (iterator.hasNext()) iterator.next().getBasicRemote().sendText(buildJsonMessageData(username, message));
+		}
+	}
+	@OnClose
+	public void handleClose(Session userSession) throws IOException {
+		chatroomUsers.remove(userSession);
+		Iterator<Session> iterator = chatroomUsers.iterator();
+        while (iterator.hasNext()) (iterator.next()).getBasicRemote().sendText(buildJsonUsersData());
+	}
+	private  String buildJsonUsersData() { 
+		Iterator<String> iterator = getUserNames().iterator();
+		JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
+		while (iterator.hasNext()) jsonArrayBuilder.add((String)iterator.next());
+		return Json.createObjectBuilder().add("users", jsonArrayBuilder).build().toString();
 	}
 	
+	private String buildJsonMessageData(String username, String message) {  
+		JsonObject jsonObject = Json.createObjectBuilder().add("message", username+": "+message).build();
+		StringWriter stringWriter = new StringWriter();
+		try (JsonWriter jsonWriter = Json.createWriter(stringWriter)) {jsonWriter.write(jsonObject);}
+		return stringWriter.toString();
+	}
+	private Set<String> getUserNames(){
+		HashSet<String> returnSet = new HashSet<String>();
+		Iterator<Session> iterator = chatroomUsers.iterator();
+    	while (iterator.hasNext()) returnSet.add(iterator.next().getUserProperties().get("username").toString());
+		return returnSet;
+	}
 }
